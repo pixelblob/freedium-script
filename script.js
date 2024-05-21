4db465a783e691680b5497bc88f8954cc3ace21a
pixelblob <phillipsfamily.ethan@gmail.com>
2024-05-21 23:29:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
 // @author       Pixelblob
 // @match        https://medium.com/*
 // @include      https://*.medium.com/*
+// @include      https://*.plainenglish.io/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=medium.com
 // @grant        none
 // ==/UserScript==
